8d40281921213c6eaa474d6ae617b605b5552f25
jonathanvazquez16 <jonathan.vazquez16@upr.edu>
2024-08-23 16:55:47 +0000

    commit

---
+++ lab01.cpp
@@ -0,0 +1,22 @@
+// Jonathan Vazquez
+// A program practice
+
+#include<iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello World!" << endl;
+    cout << "Hello Solar System!" <<endl;
+    cout << "Hello Galaxy!" << endl;
+    cout << "Hello Universe!" << endl;
+    cout << "Hello ...?" << endl;
+
+    cout << "This is a 1" << endl;
+    cout << "This is a 2" << endl;
+    cout << "This is a 3" << endl;
+    cout << "This is a 4" << endl;
+    cout << "This is a 5" << endl;
+    cout << "Bye World" << endl;
+
+    return 0;
+}
--- main.cpp
+++ main.cpp
@@ -8,6 +8,6 @@ int main() {
     number1=12;
     number2=13;
     sum = number1 + number2;
-    std::cout<< "The sum of these two integers are "<< sum << std::endl;
+    std::cout << "The sum of these two integers are "<< sum << std::endl;
     return 0; 
 }
