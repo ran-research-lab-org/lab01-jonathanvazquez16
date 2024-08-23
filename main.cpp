b73fe5db6b3b01f557c8004689d6a13e65744c74
jonathanvazquez16 <jonathan.vazquez16@upr.edu>
2024-08-23 16:26:43 +0000

    commit

--- main.cpp
+++ main.cpp
@@ -3,5 +3,11 @@
 #include <iostream>
 
 int main() {
-  std::cout << "Hello Jonathan!\n";
+    int number1, number2, sum;
+
+    number1=12;
+    number2=13;
+    sum = number1 + number2;
+    std::cout<< "The sum of these two integers are"<< sum << std::endl;
+    return 0;
 }
